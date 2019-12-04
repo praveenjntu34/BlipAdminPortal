68f8a7960b95ab9e8eb6ad44c8843c052b2c3cf5
Praveen Vatambeti <praveen@nineleaps.com>
2019-12-04 14:10:53 +0530

    added dashboard module

--- src/app/dashboard-menu/dashboard-menu.module.ts
+++ src/app/dashboard-menu/dashboard-menu.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MenuRoutingModule } from './dashboard-menu.routing.module';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, MenuBarComponent],
   imports: [
     CommonModule,
     MenuRoutingModule

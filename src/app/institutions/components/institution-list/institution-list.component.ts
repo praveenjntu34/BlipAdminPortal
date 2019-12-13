3914ec13f7c981db81eaa0b619a0268e5e0f6988
Praveen Vatambeti <praveen@nineleaps.com>
2019-12-13 17:03:48 +0530

    added pincode filters

--- src/app/institutions/components/institution-list/institution-list.component.ts
+++ src/app/institutions/components/institution-list/institution-list.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-institution-list',
-  templateUrl: './institution-list.component.html',
-  styleUrls: ['./institution-list.component.css']
+  templateUrl: './institution-list-new.component.html',
+  styleUrls: ['./institution-list-new.component.css']
 })
 export class InstitutionListComponent implements OnInit {
 

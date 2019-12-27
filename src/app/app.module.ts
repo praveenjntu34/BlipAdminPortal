4db1604d418592f53659f804253f0ce75767a047
Praveen <praveenjntu34@gmail.com>
2019-12-28 00:23:09 +0530

    added super modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { appRoutes } from './app.routing';
 import { BlipLayoutComponent } from './blip-layout/blip-layout/blip-layout.component';
 import { DashboardMenuModule } from './dashboard-menu/dashboard-menu.module';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { HttpClientModule } from '@angular/common/http'
     BrowserModule,
     DashboardMenuModule,
     HttpClientModule,
+    FormsModule,
+    NgbModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/institutions/components/institution-list/institution-list.component.ts
+++ src/app/institutions/components/institution-list/institution-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { from } from 'rxjs';
 import { Institutions } from './institution.data'
+import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-institution-list',
@@ -13,6 +14,41 @@ export class InstitutionListComponent implements OnInit {
 
   name:string;
 
+  title = 'ng-bootstrap-modal-demo';
+  closeResult: string;
+  modalOptions:NgbModalOptions;
+ 
+  constructor(
+    private modalService: NgbModal
+  ){
+    this.modalOptions = {
+      backdrop: 'static',
+      centered: true,
+      backdropClass:'customBackdrop',
+      size: 'xl',
+      windowClass: 'modal-container'
+    }
+  }
+  
+  open(content) {
+    
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+ 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
   institutions = [
     new Institutions("Newyork University","Last active 15 Oct 2:00 PM","Bolshoy prospect,Petrogradskaya storona,12B","../../../../assets/newyork.jpg",28),
     new Institutions("MIT University","Last active 15 Oct 2:00 PM","Bolshoy prospect,Petrogradskaya storona,12B","../../../../assets/newyork.jpg",28),
@@ -24,8 +60,6 @@ export class InstitutionListComponent implements OnInit {
     new Institutions("Newyork University","Last active 15 Oct 2:00 PM","Bolshoy prospect,Petrogradskaya storona,12B","../../../../assets/newyork.jpg",28),
     new Institutions("Newyork University","Last active 15 Oct 2:00 PM","Bolshoy prospect,Petrogradskaya storona,12B","../../../../assets/newyork.jpg",28),
   ]
-  constructor() {
-  }
 
   ngOnInit() {
   }
